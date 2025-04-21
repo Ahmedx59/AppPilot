994508ba9d7e76ffedcae973fa6eaaebd4f99dd4
Ahmedx59 <Ahmedx59@gmail.com>
2025-04-21 15:36:16 +0200

    add admin data

--- apps/stores/admin.py
+++ apps/stores/admin.py
@@ -1,1 +1,43 @@
 from django.contrib import admin
+
+from apps.stores.models import (
+    Store,
+    Category,
+    AppSection,
+    AppTemplate,
+    StorTemplates,
+    )
+
+@admin.register(Store)
+class AdminStore(admin.ModelAdmin):
+    list_display = ('user','name')
+    list_filter = ('user','name')
+
+
+@admin.register(Category)
+class AdminCategory(admin.ModelAdmin):
+    list_display = ('name',)
+    list_filter = ('name',)
+
+
+
+
+@admin.register(AppSection)
+class AdminAppSection(admin.ModelAdmin):
+    list_display = ('name',)
+    list_filter = ('name',)
+
+
+
+@admin.register(AppTemplate)
+class AdminAppSection(admin.ModelAdmin):
+    list_display = ('name',)
+    list_filter = ('name',)
+
+
+
+
+@admin.register(StorTemplates)
+class AdminStorTemplates(admin.ModelAdmin):
+    list_display = ('name',)
+    list_filter = ('name',)
