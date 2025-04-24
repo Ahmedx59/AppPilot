71efad3d4e199fef62c81cdc9e5d81394a17c153
Ahmedx59 <Ahmedx59@gmail.com>
2025-04-24 22:34:48 +0200

    add TabularInline im admin

--- apps/stores/admin.py
+++ apps/stores/admin.py
@@ -6,23 +6,31 @@ from apps.stores.models import Category
 from apps.stores.models import Store
 from apps.stores.models import StorTemplates
 
+class AppTemplateInline(admin.TabularInline):
+    model = AppTemplate
+
+
+class AppSectionInline(admin.TabularInline):
+    model = AppSection
 
 @admin.register(Store)
 class AdminStore(admin.ModelAdmin):
     list_display = ("user", "name")
     list_filter = ("user", "name")
 
-
 @admin.register(Category)
 class AdminCategory(admin.ModelAdmin):
     list_display = ("name",)
     list_filter = ("name",)
+    inlines = (AppSectionInline,)
 
 
 @admin.register(AppSection)
 class AdminAppSection(admin.ModelAdmin):
     list_display = ("name",)
     list_filter = ("name",)
+    inlines = (AppTemplateInline,)
+
 
 
 @admin.register(AppTemplate)
