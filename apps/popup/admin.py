a68519693e862c3d47e6230ebb153d9a99420d2f
Ahmedx59 <Ahmedx59@gmail.com>
2025-04-30 20:57:35 +0300

    add model in popup models and add admin models

--- apps/popup/admin.py
+++ apps/popup/admin.py
@@ -1,1 +1,16 @@
 from django.contrib import admin
+
+from apps.popup.models import Popup
+from apps.popup.models import StorePopup
+
+
+@admin.register(StorePopup)
+class AdminStorePopup(admin.ModelAdmin):
+    list_display = ("store_id", "name", "active")
+    list_filter = ("store_id", "name", "active")
+
+
+@admin.register(Popup)
+class AdminPopup(admin.ModelAdmin):
+    list_display = ("name",)
+    list_filter = ("name",)
--- apps/popup/models.py
+++ apps/popup/models.py
@@ -1,1 +1,34 @@
 from django.db import models
+
+from apps.stores.models import Store
+
+
+class StorePopup(models.Model):
+    store_id = models.ForeignKey(
+        Store,
+        related_name="store_popup",
+        on_delete=models.CASCADE,
+    )
+    plugin_id = ""
+    page_url = ""
+    name = models.CharField(max_length=50)
+    setting = models.JSONField()
+    active = models.BooleanField(default=False)
+    updated_at = ""
+
+    def __str__(self):
+        return self.name
+
+
+class Popup(models.Model):
+    store_popup = models.ForeignKey(
+        StorePopup,
+        related_name="popup",
+        on_delete=models.CASCADE,
+    )
+    name = models.CharField(max_length=50)
+    default_setting = models.JSONField()
+    code = models.CharField()
+
+    def __str__(self):
+        return self.name
