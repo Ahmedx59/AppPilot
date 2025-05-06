827a9ea05b65d633ecfaf9f4f43732e95e5d1fb7
Ahmedx59 <Ahmedx59@gmail.com>
2025-05-07 00:12:14 +0300

    add action duplicate and perform_destroy

--- apps/notification/api/views.py
+++ apps/notification/api/views.py
@@ -1,6 +1,11 @@
+from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.serializers import ValidationError
 
-from apps.notification.api.serializers import NotificationSerializer , UpdateCreateNotificationSerializer
+from apps.notification.api.serializers import NotificationSerializer
+from apps.notification.api.serializers import UpdateCreateNotificationSerializer
 from apps.notification.models import Notification
 
 
@@ -17,4 +22,31 @@ class NotificationViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         user_store = self.request.user.store
         return super().get_queryset().filter(store=user_store)
-    
+
+    def perform_destroy(self, instance):
+        if instance.is_published:
+            raise ValidationError(
+                {"message": "A published notification cannot be deleted."},
+            )
+        instance.delete()
+        return Response(
+            {"message": "Notification deleted successfully."},
+            status=status.HTTP_200_OK,
+        )
+
+    @action(detail=True, methods=["post"])
+    def duplicate(self, request, pk=None):
+        notification = self.get_object()
+        if notification.is_published:
+            return Response(
+                {
+                    "message": (
+                        "This notification is already published and cannot be copied."
+                    ),
+                },
+                status=status.HTTP_200_OK,
+            )
+        notification.pk = None
+        notification.save()
+        serializer = self.get_serializer(notification)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
