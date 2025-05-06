fcdd492d6df334ae3baab40d371bd89d08e0eb11
Ahmedx59 <Ahmedx59@gmail.com>
2025-05-07 00:12:29 +0300

    add update in UpdateCreateNotificationSerializer

--- apps/notification/api/serializers.py
+++ apps/notification/api/serializers.py
@@ -6,15 +6,21 @@ from apps.notification.models import Notification
 class NotificationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notification
-        fields = ("title", "message", "url", "sent_at", "created_at","is_published",)
+        fields = ("title", "message", "url", "sent_at", "created_at", "is_published")
 
 
 class UpdateCreateNotificationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notification
-        fields = ("title","message","url",)
+        fields = ("title", "message", "url")
 
-        
     def create(self, validated_data):
         validated_data["store"] = self.context["request"].user.store
         return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        if instance.is_published:
+            raise serializers.ValidationError(
+                {"message": ("A published notification cannot be edited.")},
+            )
+        return super().update(instance, validated_data)
