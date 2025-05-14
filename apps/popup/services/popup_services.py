2f0ce962e83fd7c29d93f8c2b4a7b14b8e80da9e
Ahmedx59 <Ahmedx59@gmail.com>
2025-05-14 18:58:32 +0300

    .

--- apps/popup/services/popup_services.py
+++ apps/popup/services/popup_services.py
@@ -13,29 +13,3 @@ class PopupStoreServices:
             ).count()
             if active_popup >= 3:  # noqa:PLR2004
                 raise ValidationError({"active": "you can activate 3 popup only."})
-
-
-# class PopupStoreServices:
-
-#     # def __init__(self, outer_queryset):
-#     #     self.queryset = outer_queryset
-
-
-#     @classmethod
-#     def activate(cls, store_popup ,hjf):
-#         cls.test()
-#         cls.generate(store_popup)
-#         # self.queryset.filter(name="ds")
-
-
-#     def test(self):
-#         print(self.queryset)
-
-
-#     @staticmethod
-#     def generate(store_popup):
-#          if store_popup.active:
-#             active_popup = StorePopup.objects.filter
-#               (active=True , store = store_popup.store).count()
-#             if active_popup >= 3:
-#                 raise ValidationError({"active": "you can activate 3 popup only."})
