a705709b66f13d393fcde9748c57fa8c50be2710
Ahmedx59 <Ahmedx59@gmail.com>
2025-05-21 02:10:00 +0300

    per visits (day , manth , today , month total)

--- apps/stores/services/store_services.py
+++ apps/stores/services/store_services.py
@@ -1,9 +1,9 @@
 from datetime import timedelta
 
 from django.core.exceptions import ValidationError
+from django.db.models import Sum
 from django.shortcuts import get_object_or_404
 from django.utils.timezone import now
-from django.db.models import Sum
 
 from apps.stores.models import AppTemplate
 from apps.stores.models import Store
@@ -118,7 +118,7 @@ class VisitService:
     def visits_store(cls, user_store):
         today = now().date()
 
-        last_days = [today - timedelta(days=x) for x in range(1,8)]
+        last_days = [today - timedelta(days=x) for x in range(1, 8)]
 
         visits = Visit.objects.filter(store=user_store, date__in=last_days)
 
@@ -142,19 +142,17 @@ class VisitService:
                     },
                 )
         return result
-    
+
     @classmethod
     def visits_store_month(cls, user_store):
         today = now().date()
 
-        last_days = [today - timedelta(days=x) for x in range(1,31)]
+        last_days = [today - timedelta(days=x) for x in range(1, 31)]
 
         visits = Visit.objects.filter(store=user_store, date__in=last_days)
 
         result = []
-        x = 0
-        for day in last_days:
-            x += 1
+        for _x, day in enumerate(last_days, start=1):
             visits_by_date = visits.filter(date=day).first()
 
             if visits_by_date:
@@ -172,24 +170,21 @@ class VisitService:
                         "count": 0,
                     },
                 )
+        return result
 
-        result.append(
-            {
-                "count":x,
-            }
-        )
-        return result 
-    
     @classmethod
     def visits_today(cls, user_store):
         today = now().date()
         visit = Visit.objects.filter(store=user_store, date=today).first()
-        return {"date": today, "count": visit.count if visit else 0}
+        return visit.count if visit else 0
 
     @classmethod
     def total_month_count(cls, user_store):
         today = now().date()
         first_day = today - timedelta(days=30)
-        visits = Visit.objects.filter(store=user_store, date__gte=first_day, date__lte=today)
-        total = visits.aggregate(total=Sum("count"))["total"] or 0
-        return total
+        visits = Visit.objects.filter(
+            store=user_store,
+            date__gte=first_day,
+            date__lte=today,
+        )
+        return visits.aggregate(total=Sum("count"))["total"] or 0
